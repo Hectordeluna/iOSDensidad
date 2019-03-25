9f69de87f1fedb99432091cd719295c60f0bed09
Rafael Serna <rafaelserna@rafaels-macbook-pro.local>
2019-03-24 18:56:19 -0600

    Initial Commit

---
+++ Densidad/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Densidad
+//
+//  Created by Rafael Serna on 3/24/19.
+//  Copyright © 2019 Rafael Serna. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
