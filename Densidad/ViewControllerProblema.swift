c1827503b9d25b31b0946cefb191b9e4267d390f
Rafael Serna <rafaelserna@Rafaels-MacBook-Pro.local>
2019-03-24 19:42:46 -0600

    UI inicial

---
+++ Densidad/ViewControllerProblema.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerProblema.swift
+//  Densidad
+//
+//  Created by Rafael Serna on 3/24/19.
+//  Copyright © 2019 Rafael Serna. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerProblema: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
